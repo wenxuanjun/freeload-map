61ce10bbe4fe222ae1a7b0ae11df194235b1b8fb
wenxuanjun <wendster@163.com>
2021-08-27 11:56:41 +0800

    Add: Bad performance

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import AmapVueConfig from '@amap/amap-vue/lib/config'
-import { MdButton, MdCard } from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
-Vue.use(MdButton)
-Vue.use(MdCard)
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 AmapVueConfig.key = 'ed5ff946c6eaa36a327380c3b57deff2'
