0aa9eb71aa9ff5734c8ca3d54177b849fdc89fee
wenxuanjun <wendster@163.com>
2021-08-30 08:10:53 +0800

    New: Bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   productionSourceMap: false,
   css: {
@@ -10,5 +13,25 @@ module.exports = {
     compress: true,
     disableHostCheck: true
   },
-  transpileDependencies: [                                                                                                  'vuetify'                                                                                                             ]
+  transpileDependencies: [
+    'vuetify'
+  ],
+  configureWebpack: {
+    plugins: [
+      new TerserPlugin({
+        parallel: 4,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log']
+          }
+        }
+      }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'report.html'
+      })
+    ]
+  }
 }
